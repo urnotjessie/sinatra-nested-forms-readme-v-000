52580f7ab9af0010fe6a79c5b5c2f6a82163fab1
urnotjessie <zg2220@columbia.edu>
2018-06-16 23:39:35 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,4 +2,19 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/' do
+    erb :new
+  end
+
+  get 'student' do
+    @student = Student.new(params[:student])
+
+    params[:student][:courses].each do |details|
+      Couse.new(details)
+    end
+
+    @courses  = Course.all
+
+    erb :student
+  end
 end
